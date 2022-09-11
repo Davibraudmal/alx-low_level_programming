dca9e58d0bebf96df3e5fe26f29443788276a28e
davibraudmal <davidibrahimmalgwi@gmail.com>
2022-09-11 15:02:08 -0700

    my first code in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+nclude<stdio.h>
+
+int main()
+
+{
+
+		//my first c code
+
+ 	  char msg[] = "Programming is like building a multilingual puzzle";
+
+        
+
+            puts(msg); 
+	    
+	    //the above will write the string to stdout
+                
+
+                    return 0;
+
+          }
