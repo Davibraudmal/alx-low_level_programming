196543dcccb369fd40d3300a50f5e37956a2db32
davibraudmal <davidibrahimmalgwi@gmail.com>
2022-09-12 02:39:45 -0700

    i think this is the last update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*this code uses puts instead of printf*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle"\n);
 	return (0);
 }
