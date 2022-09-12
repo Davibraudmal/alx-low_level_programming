ef139b0108a5a05b1fd04e9ddb51c275300c9385
davibraudmal <davidibrahimmalgwi@gmail.com>
2022-09-12 03:24:30 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /*this code uses puts instead of printf*/
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle"\n);
+int main(void) /*fisrt code line*/
+{/*main code boby*/
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
