61caec5b9fdaab0db8f8690f80e156ca4cfa0d67
davibraudmal <davidibrahimmalgwi@gmail.com>
2022-09-14 10:03:31 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * Prints out the last part of a quot:
- */
+* main - Prints out the last part of a quote in the standard error.
+* Return: 1 if success.
+*/
 #include <unistd.h>
 int main(void)
+/* it starts here */
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+/* this will return 1. */
 }
